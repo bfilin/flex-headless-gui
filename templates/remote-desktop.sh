05d2164e35565f310956efe5d1333fafa9996026
bfilin <network.project.9@gmail.com>
2022-10-05 11:13:32 -0400

    async

--- templates/remote-desktop.sh
+++ templates/remote-desktop.sh
@@ -2,6 +2,7 @@
 killall -q -u ubuntu gnome-keyring-daemon --wait
 echo -n testing123 | /usr/bin/gnome-keyring-daemon --daemonize --login 
 systemctl --user restart gnome-remote-desktop.service
-
+# the above works for graphical.target
+# Note: for multi-user.target enable the following line - needs testing
 # gnome-shell --wayland  --headless --virtual-monitor 1280x1024 --no-x11
 
